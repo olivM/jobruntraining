303b1640f2b3d87f165865ab867824363390c5bb
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2012-07-31 20:18:22 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobruntraining::Application.config.secret_token = 'b64008a34b49a92bbdce68e55e7d9908055c8370865a18bc68e4988804f3f594488caaa4ead91d236657539de3a4db342925b3ed65206142fddd0eae6ae13271'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jobruntraining::Application.config.session_store :cookie_store, key: '_jobruntraining_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jobruntraining::Application.config.session_store :active_record_store
